5c3cbf3f2ba785aad3e83e2c97bcb269c8ca30e6
confused-cat <2155598746@qq.com>
2026-03-19 10:58:37 +0800

    app root

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -10,6 +10,7 @@ const router = useRouter();
 async function handelSession() {
     const loading = await http.get("/auth/session");
     const data = loading.data.data.data;
+    console.log(data);
     if (data) {
         router.push("/");
     }
